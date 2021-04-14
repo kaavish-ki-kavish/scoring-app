e68bedb20096008d2cade4c7f4fa5029afe8761e
Mudasir Hanif <ms03831@st.habib.edu.pk>
2021-04-14 22:35:51 +0500

    Update data.py

--- data.py
+++ data.py
@@ -15,7 +15,7 @@ for (i, row) in df.iterrows():
     img_path = filepath + row['img_paths']
     num_results = ImageScore.objects.filter(image_path = img_path).count()
     if num_results == 0:
-        scorer = random.choice(SCORERS)
-        image_obj = ImageScore.objects.create(image_path = img_path, scorer = scorer)
-        scorer = random.choice(SCORERS)
-        image_obj = ImageScore.objects.create(image_path = img_path, scorer = scorer)
+        scorer1 = random.choice(SCORERS)
+        image_obj = ImageScore.objects.create(image_path = img_path, scorer = scorer1)
+        scorer2 = random.choice(list(set(SCORERS) - set([scorer1])))
+        image_obj = ImageScore.objects.create(image_path = img_path, scorer = scorer2)
